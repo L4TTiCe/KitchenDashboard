647e0c3a840725833ca8b337da36e00c3f5a3e1e
Dheshan Mohandass <dheshan.mail@gmail.com>
2022-03-04 20:43:55 -0500

    Added Database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,37 @@
+/**
+ * @file server.ts serves as the entry point to the app
+ */
 import express, {Request, Response} from "express";
+import mongoose from "mongoose"
+
+/**
+ * Connects to the Mongo Database with db connection details from Environment Variables
+ */
+const connectDatabase = (): void => {
+    // Connecting to DB
+    let dbUri: string = process.env.MONGODB_PREFIX + "://" + process.env.MONGODB_USER
+        + ":" + process.env.MONGODB_PASSWORD + "@" + process.env.MONGODB_HOST;
+    if (process.env.MONGODB_PORT) {
+        dbUri = dbUri + process.env.MONGODB_PORT;
+    }
+    console.log("Inferred DB_URI: " + process.env.MONGODB_PREFIX + "://" + process.env.MONGODB_USER
+        + ":*****@" + process.env.MONGODB_HOST);
+
+    mongoose.connect(dbUri)
+        .catch((err: object) => {
+            console.log(err);
+        });
+    mongoose.connection.once("open", () => {
+        console.log("MongoDB connection established successfully");
+    });
+}
 
 /**
  * Starts the Server and listens on the specified PORT
  * @param port {string | number} - the port the server will listen on
  */
 const startServer = (port: string | number): void => {
+    connectDatabase()
     const app = express();
 
     app.get("/up", (req: Request, res: Response) =>
