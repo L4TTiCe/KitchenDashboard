c4b2e4fcd74eed09696772686d4186e9cf3edb65
Dheshan Mohandass <dheshan.mail@gmail.com>
2022-04-14 20:09:49 -0400

    Expanded Nutrition Schema
    Signed-off-by: Dheshan Mohandass <dheshan.mail@gmail.com>

--- src/models/Nutrition.ts
+++ src/models/Nutrition.ts
@@ -11,6 +11,14 @@ export interface Nutrition {
     serving_unit: string;
     calories: number;
     total_fat: number;
-    carbohydrates: number;
+    saturated_fat: number;
+    trans_fat: number;
+    cholesterol: number;
+    sodium: number;
+    total_carbohydrates: number;
+    dietary_fiber: number;
+    total_sugar: number;
+    added_sugar: number;
     protein: number;
+    description: string;
 }
